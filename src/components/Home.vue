4b746398f8d9d9c1bb9563cb483d6fff4ae21bad
Valeriy Kostikov <mkoctuk@gmail.com>
2017-11-16 14:55:01 +0300

    add home and 404 page

---
+++ src/components/Home.vue
@@ -0,0 +1,15 @@
+<template>
+</template>
+
+<script>
+export default {
+  name: 'Home',
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,22 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Home from '@/components/Home'
+import NotFoundComponent from '@/components/NotFoundComponent'
 
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Home',
+      component: Home
+    },
+    {
+      path: '*',
+      name: '404',
+      component: NotFoundComponent
     }
   ]
 })
